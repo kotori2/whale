b1cde54f16d4ed3bf7119201360db6fa699fafda
kotori2 <admin@0x.mk>
2019-09-15 18:10:51 +0800

    修复在部分设备上没有权限的问题

--- whale/src/dbi/arm64/inline_hook_arm64.cc
+++ whale/src/dbi/arm64/inline_hook_arm64.cc
@@ -1,4 +1,5 @@
 #include <sys/mman.h>
+#include <errno.h>
 #include "platform/memory.h"
 #include "dbi/arm64/inline_hook_arm64.h"
 #include "dbi/arm64/registers_arm64.h"
@@ -8,7 +9,7 @@
 #include "base/logging.h"
 
 #define __ masm.
-
+extern int errno;
 namespace whale {
 namespace arm64 {
 
@@ -25,6 +26,27 @@ void Arm64InlineHook::StartHook() {
     masm.FinalizeCode();
 
     size_t backup_size = masm.GetSizeOfCodeGenerated();
+
+    //change memory attr
+    auto page = GetTarget<u4 *>();
+    __android_log_print(ANDROID_LOG_INFO, "Whale", "MProtect address %p, length %zu", page, backup_size);
+    size_t page_size = sysconf(_SC_PAGE_SIZE);
+    
+    //align
+    auto offset = reinterpret_cast<size_t>(page) % page_size;
+    if(offset) {
+        page = reinterpret_cast<u4 *>(reinterpret_cast<size_t>(page) - offset);
+    }
+    
+    __android_log_print(ANDROID_LOG_INFO, "Whale", "MProtect paged address %p, length %zu", page, page_size);
+
+    errno = 0;
+    int stat = mprotect(page, page_size, PROT_READ|PROT_WRITE|PROT_EXEC);
+    __android_log_print(ANDROID_LOG_INFO, "Whale", "MProtect result: %d", stat);
+    if(stat != 0){
+        __android_log_print(ANDROID_LOG_ERROR, "Whale", "MProtect error: %s", strerror(errno));
+    }
+
     backup_code_ = new BackupCode(GetTarget<u4 *>(), backup_size);
 
     if (backup_ != nullptr) {
